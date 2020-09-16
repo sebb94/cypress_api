b13c60a9b4fcf57ae42d59a0e3c8cabc640c24a3
sebb94 <omgl0lwth@gmail.com>
2020-09-16 09:41:38 +0200

    validate headers

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,4 +1,9 @@
-describe('test', () => {
-    it('test', () => {
+describe('REST API with Cypress', () => {
+    it('API Test - Validate Headers', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.get('@pokemon')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
     });
 });
